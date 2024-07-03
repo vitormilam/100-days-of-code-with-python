5a594138d663d8ef73f4540451d102f818e55506
vitormilam <vijmilam@gmail.com>
2024-07-03 16:23:57 -0300

    DAY-1

---
+++ DAY-1/Day_1_exercise_3.py
@@ -0,0 +1,10 @@
+#Exercise Fix the code below 3
+#print(Day 1 - String Manipulation")
+#print("String Concatenation is done with the "+" sign.")
+  #print('e.g. print("Hello " + "world")')
+#print(("New lines can be created with a backlash and n.")
+
+print("Day 1 - String Manipulation:")
+print("String Concatenation is done with the '+' sign.")
+print('e.g print("Hello " + "World)')
+print("New lines can be created with a backslash and n.")
---
+++ DAY-1/Day_1_exercise_4.py
@@ -0,0 +1,9 @@
+#Exercise Band Name Generator 4
+
+print("Welcome to the Band Name Generator.")
+
+name_cidade = input("What's the name of the city you grew up in? \n")
+
+name_pet = input("What's your pet's name? \n")
+
+print(f"Your band name could be {name_cidade}{name_pet} ")
